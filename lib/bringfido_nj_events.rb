a2e8fde59d2a0bc6027d786e9cef512d196d0b2f
Gina Graci <ginagraci92@gmail.com>
2019-12-06 06:06:01 +0000

    first commit

---
+++ lib/bringfido_nj_events.rb
@@ -0,0 +1,6 @@
+require "bringfido_nj_events/version"
+
+module BringfidoNjEvents
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/bringfido_nj_events/version.rb
@@ -0,0 +1,3 @@
+module BringfidoNjEvents
+  VERSION = "0.1.0"
+end
